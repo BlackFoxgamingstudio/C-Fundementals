ca45564d28c9f63fecaee20f774fb42175ed11f7
naruto <blackfoxgamingstudio@gmail.com>
2017-03-19 11:00:48 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WOnderLandCalculator
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Start:
+			int num01;
+			int num02;
+		
+			Console.Write("type a number to be multiplied: ");
+			num01 = Convert.ToInt32 (Console.ReadLine());
+			Console.Write("type a second number to be multiplied: ");
+		    num02 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("the result is " + num01 * num02);
+			Console.ReadKey();
+			goto Start; //jumps to "Start:"
+		}
+	}
+}
